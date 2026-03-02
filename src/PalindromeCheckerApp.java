8d0f421344140afb25ce644157936de378cb6353
gnaneswar944 <gk7408@srmist.edu.in>
2026-03-02 09:34:47 +0530

    UC10 Normalized palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,20 +1,19 @@
 import java.util.*;
 public class PalindromeCheckerApp{
-    private static boolean Check(String s,int start ,int end){
-        if(start >= end){
-            return true;
-        }
-        if(Character.toLowerCase(s.charAt(start)) != Character.toLowerCase(s.charAt(end)) ){
-            return false;
-        }
-        return Check(s,start +1,end -1);
-
-    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a string:");
         String input = sc.nextLine();
-        boolean isPalindrome = Check(input,0,input.length()-1);
+        String normalized = input.replaceAll("\\s","").toLowerCase();
+        boolean isPalindrome = true;
+        for(int i=0;i<normalized.length()/2;i++){
+            if (normalized.charAt(i) != normalized.charAt(normalized.length() -1 -i)){
+                isPalindrome = false;
+                break;
+
+            }
+        }
+
         if(isPalindrome){
             System.out.println("true");
         }
